96e7de0c9f6c520cd9126dc1e3c65e50a2bd41cd
Valentina Shutkina <valentina.shutkina@icloud.com>
2022-08-24 23:11:30 +0300

    Second lesson exercises are added

---
+++ src/main/java/Circle.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Circle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите координаты точки по оси х:");
+        int a = scanner.nextInt();
+        System.out.println("Введите координаты точки по оси y");
+        int b = scanner.nextInt();
+        System.out.println("Введите радиус круга r:");
+        int r = scanner.nextInt();
+        System.out.println("Введите координаты центра круга по оси x:");
+        int x = scanner.nextInt();
+        System.out.println("Введите координаты центра круга по оси y:");
+        int y = scanner.nextInt();
+        isPointLocatedInsideCircle(x, y, r, a, b);
+    }
+
+    public static void isPointLocatedInsideCircle(int x, int y, int r, int a, int b) {
+        if ((x - a) * (x - a) + (y - b) * (y - b) <= r * r) {
+            System.out.println("Точка находится внутри круга");
+        } else {
+            System.out.println("Точка находится вне круга");
+        }
+    }
+}
